253227451d18baf2159a73a4d730c5b146d59b0e
Andrew <121983190+Andrew-laravel-php@users.noreply.github.com>
2023-05-11 05:56:10 +0300

    Upgrade k neighbors and + vectors
    Upgrade k neighbors and + vectors

--- app.py
+++ app.py
@@ -39,19 +39,39 @@ def sbc():
 @app.route('/suggest')
 def suggest():
   query = request.args.get('q')
-  suggestions = movies_df[movies_df['Title'].str.contains(query, case=False)]['Title'].tolist()
+  suggestions = movies_df[movies_df['Title'].str.contains(query, case=False, regex=False)]['Title'].tolist()
   return jsonify(suggestions)
 
 @app.route('/search')
 def search():
+    #извлекает параметр 'query' из GET-запроса (если он был передан), который содержит поисковой запрос, и присваивает его переменной query.
     query = request.args.get('query')
+    #если query отсутствует или имеет пустое значение, возвращает сообщение об ошибке в формате JSON.
     if not query:
         return jsonify({'error': 'query parameter is missing'})
+    #используя модель Doc2Vec, получает вектор запроса movie_vector, преобразовав текст запроса query в список слов и вызвав метод infer_vector
     movie_vector = doc2vec_model.infer_vector(query.split())
-    nearest_neighbors, distances = annoy_index.get_nns_by_vector(movie_vector, 10, include_distances=True)
+    #используя библиотеку ANNOY, находит 10 ближайших соседей для вектора movie_vector в индексе annoy_index. Параметр include_distances=True запрашивает возвращение расстояний до каждого соседа, которые присваиваются переменной distances.
+    nearest_neighbors, distances = annoy_index.get_nns_by_vector(movie_vector, 5, search_k=500, include_distances=True)
+    #используя пандас, извлекает из датафрейма movies_df названия и постеры 10 фильмов, соответствующих ближайшим соседям, и присваивает их переменной 
     similar_movies = movies_df.iloc[nearest_neighbors][['Title', 'Poster']]
+    #добавляет новый столбец Distance в similar_movies и присваивает ему значения расстояний distances.
     similar_movies['Distance'] = distances
+    # определение градации цвета в зависимости от расстояния
+    colors = []
+    for distance in distances:
+        if distance < 0.2:
+            colors.append('green')
+        elif distance < 0.4:
+            colors.append('yellow')
+        elif distance < 0.6:
+            colors.append('orange')
+        else:
+            colors.append('red')
+    similar_movies['Color'] = colors
+    #преобразует датафрейм similar_movies в формат JSON, используя метод to_dict, и возвращает результат поиска в формате JSON.
     return jsonify(similar_movies.to_dict(orient='records'))
+
 
 
 
--- film.py
+++ film.py
@@ -9,7 +9,7 @@ df = pd.read_csv('movies.csv')
 documents = [TaggedDocument(words=word_tokenize(title.lower()), tags=[str(index)]) for index, title in df['Title'].items()]
 
 # Обучение модели Doc2Vec
-model = Doc2Vec(documents, vector_size=100, window=5, min_count=1, workers=4, epochs=20)
+model = Doc2Vec(documents, vector_size=300, window=5, min_count=1, workers=4, epochs=20)
 
 # Сохранение модели в файл
 model.save('doc2vec.model')
