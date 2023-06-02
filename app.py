32d3799bf4e4a95c7f09e28db28ddfb8990f472b
Andrew <121983190+Andrew-laravel-php@users.noreply.github.com>
2023-06-03 01:54:53 +0300

    Time to Added HQ GENRE's
    1. Time to add genre from films
    2. Time to add database .... but have any problems with sqllite :interrobang:

--- app.py
+++ app.py
@@ -1,15 +1,52 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, url_for, flash, redirect
 import pandas as pd
 import gensim
 from gensim.models import Doc2Vec
 from annoy import AnnoyIndex
 from sklearn.feature_extraction.text import TfidfVectorizer
-import pandas as pd
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
 
 # Загрузка данных из файла CSV
 data = pd.read_csv('movies.csv')
 # Создание и инициализация векторизатора
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'apetiajiet1u351358paiX14u1390@1!'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///filmdb.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Class for form registration
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+# # Model for user creation
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(80), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(80), nullable=False)
+#     pass_hash = db.Column(db.String(80), nullable=False)
+
+# db.create_all()
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # Добавьте здесь код для обработки данных формы регистрации
+        flash('Registration successful!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/login')
+def login():
+    return render_template('index.html')
 
 
 @app.route('/')
@@ -32,13 +69,9 @@ def search():
         return jsonify({'error': 'query parameter is missing'})
 
     movie_vector = vectorizer.transform([query]).toarray().flatten()
-
-    # Search for the nearest neighbors using the ANNOY model
     nearest_neighbors, distances = annoy_index.get_nns_by_vector(movie_vector, 5, include_distances=True)
-
     similar_movies = movies_df.iloc[nearest_neighbors][['Title', 'Poster']]
     similar_movies['Distance'] = distances
-
     colors = []
     for distance in distances:
         if distance < 0.2:
@@ -51,13 +84,14 @@ def search():
             colors.append('red')
     similar_movies['Color'] = colors
 
-    # User preferences
-    user_preferences = request.args.getlist('preferences')  # Assuming preferences are passed as a list in the request
+    user_preferences = request.args.getlist('preferences') 
 
     if user_preferences:
-        # Filter recommendations based on user preferences
         recommended_movies = filter_movies_based_on_preferences(similar_movies, user_preferences)
-        return jsonify(recommended_movies.to_dict(orient='records'))
+        if recommended_movies.empty:
+            return jsonify({'message': 'No movies found based on your preferences.'})
+        else:
+            return jsonify(recommended_movies.to_dict(orient='records'))
     else:
         return jsonify(similar_movies.to_dict(orient='records'))
 
@@ -66,10 +100,30 @@ def filter_movies_based_on_preferences(movies_df, user_preferences):
     filtered_movies = movies_df[movies_df['Genre'].isin(user_preferences)]
     return filtered_movies
 
-
 @app.route('/searchbycategory')
 def search_by_category():
-    return render_template('search_by_category.html')
+    genre = request.args.get('genre')
+    year = request.args.get('year')
+    rating = request.args.get('rating')
+
+    filtered_movies = movies_df
+
+    if genre:
+        filtered_movies = filtered_movies[filtered_movies['Genre'].str.contains(genre, case=False)]
+
+    if year:
+        filtered_movies = filtered_movies[filtered_movies['Year'] == int(year)]
+
+    if rating:
+        filtered_movies = filtered_movies[filtered_movies['IMDB Score'] >= float(rating)]
+
+    if filtered_movies.empty:
+        return jsonify({'message': 'No movies found based on the specified category.'})
+    else:
+        similar_movies = filtered_movies['Title'].tolist()
+        return render_template('search_by_category.html', similar_movies=similar_movies)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
