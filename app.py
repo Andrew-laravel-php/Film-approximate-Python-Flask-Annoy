d65c0c692a5b1c4237ed7a068584d53498e9483a
Andrew <121983190+Andrew-laravel-php@users.noreply.github.com>
2023-06-19 20:52:55 +0300

    Add first database for SQLAlchemy and SQLLITE
    1)Add first database for SQLAlchemy and SQLLITE
    2) make adaptive design
    3) make a loading screen for search request for user's

--- app.py
+++ app.py
@@ -1,15 +1,54 @@
 from flask import Flask, jsonify, request, render_template, url_for, flash, redirect
-from gensim.models import Doc2Vec
+from flask_sqlalchemy import SQLAlchemy
 from annoy import AnnoyIndex
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 import pytest 
+import time
+
+
+
 # Загрузка данных из файла CSV
 data = pd.read_csv('movies.csv')
 # Создание и инициализация векторизатора
 app = Flask(__name__)
+# SQLALCHEMY
+app.config['SECRET_KEY'] = 'atoeatoatp1395189kj@'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  # Используйте вашу базу данных
+
+db = SQLAlchemy(app)
+
+#class
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        if not username or not password:
+            flash('Пожалуйста, заполните все поля!', 'error')
+        else:
+            user = User.query.filter_by(username=username).first()
+            if user:
+                flash('Такой пользователь уже существует!', 'error')
+            else:
+                new_user = User(username=username, password=password)
+                db.session.add(new_user)
+                db.session.commit()
+                flash('Регистрация прошла успешно!', 'success')
+                return redirect('/')
+    
+    return render_template('register.html')
 
 @app.route('/')
 def index():
@@ -24,14 +63,21 @@ vectorized_data = vectorizer.fit_transform(data['Title'])
 movies_df = pd.DataFrame(data, columns=['Title', 'Poster'])
 movies_df['Genre'] = data['Genre']
 movies_df['IMDB Score'] = data['IMDB Score']
+
 @app.route('/search')
 def search():
+    start_time = time.time()
+
     query = request.args.get('query')
     if not query:
         return jsonify({'error': 'query parameter is missing'})
 
     movie_vector = vectorizer.transform([query]).toarray().flatten()
+    end_time = time.time()
+    print(f"MOVIE VECTOR 39: {end_time - start_time}")
     nearest_neighbors, distances = annoy_index.get_nns_by_vector(movie_vector, 5, include_distances=True)
+    end_time = time.time()
+    print(f"Nearest neighboor 40: {end_time - start_time}")
     similar_movies = movies_df.iloc[nearest_neighbors][['Title', 'Poster']]
     similar_movies['Distance'] = distances
     colors = []
@@ -53,9 +99,15 @@ def search():
         if recommended_movies.empty:
             return jsonify({'message': 'No movies found based on your preferences.'})
         else:
+            end_time = time.time()
+            print(f"Time to run search: {end_time - start_time}")
             return jsonify(recommended_movies.to_dict(orient='records'))
+           
     else:
+        end_time = time.time()
+        print(f"Time to run search: {end_time - start_time}")
         return jsonify(similar_movies.to_dict(orient='records'))
+    
 
 
 def filter_movies_based_on_preferences(movies_df, user_preferences):
@@ -103,5 +155,7 @@ def test_home_page():
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
 
--- film.py
+++ film.py
@@ -7,12 +7,11 @@ data = pd.read_csv('movies.csv')
 
 vectorizer = TfidfVectorizer(stop_words='english')
 vectorized_data = vectorizer.fit_transform(data['Title'])
-vectorized_data = vectorizer.fit_transform(data['Title'])
 vector_dimension = vectorized_data.shape[1]
 print(vector_dimension)
 index = AnnoyIndex(vectorized_data.shape[1], metric='angular')
 for i in range(vectorized_data.shape[0]):
     vector = vectorized_data[i].toarray().flatten()
     index.add_item(i, vector)
-index.build(n_trees=100)
+index.build(n_trees=50)
 index.save('films.ann')
